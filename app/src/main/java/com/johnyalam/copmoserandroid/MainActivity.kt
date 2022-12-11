54955bf97165b84f526d91f9e26788645e6aba1b
johnyalam <johnyalam.lut@gmail.com>
2022-12-11 07:35:56 +0200

    Initial commit

---
+++ app/src/main/java/com/johnyalam/copmoserandroid/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.johnyalam.copmoserandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.johnyalam.copmoserandroid.ui.theme.CopmoserAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CopmoserAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CopmoserAndroidTheme {
+        Greeting("Android")
+    }
+}
